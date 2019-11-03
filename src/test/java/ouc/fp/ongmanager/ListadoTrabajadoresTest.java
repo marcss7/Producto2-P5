1d22e6bbeba88bfa4daaf9e536a34ed28a08b78b
Teresa Garcia Ceva <tgarciace@fp.uoc.edu>
2019-11-03 10:58:35 +0100

    Agrego tests

--- src/test/java/ouc/fp/ongmanager/ListadoTrabajadoresTest.java
+++ src/test/java/ouc/fp/ongmanager/ListadoTrabajadoresTest.java
@@ -9,24 +9,23 @@ import javax.xml.bind.JAXBException;
 
 import org.junit.Test;
 
-//Test para probar si los objetos de la clase "Trabajador" se suman correctamente en "ListadoTrabajadores".
+// Test para probar si los objetos de la clase "Trabajador" se suman correctamente en "ListadoTrabajadores".
 public class ListadoTrabajadoresTest {
 
 	@Test
 	public void testAdd() throws JAXBException {
 		
-
-		//Inicializamos cuatro trabajadores que utilizaremos para las pruebas.
-
+		// Inicializamos cuatro trabajadores que utilizaremos para las pruebas.
 		Trabajador t1 = new Trabajador();
 		Trabajador t2 = new Trabajador();
 		Trabajador t3 = new Trabajador();
 		Trabajador t4 = new Trabajador();
 		
+		// Creamos un listado de trabajadores.
 		List<Trabajador> listadoTrabajadores = new ArrayList<Trabajador>();
 		assertEquals(0, listadoTrabajadores.size());
-		listadoTrabajadores.add(t1);
-		assertEquals(1, listadoTrabajadores.size());
+		listadoTrabajadores.add(t1); //El método "add" agrega los objetos pasados como parametros a la lista.
+		assertEquals(1, listadoTrabajadores.size()); //Al agregar una delegacion, se espera que el tamano de la lista sea 1.
 		listadoTrabajadores.add(t2);
 		assertEquals(2, listadoTrabajadores.size());
 		listadoTrabajadores.add(t3);
@@ -34,5 +33,7 @@ public class ListadoTrabajadoresTest {
 		listadoTrabajadores.add(t4);
 		assertEquals(4, listadoTrabajadores.size());
 	}
+	
+	//Si la prueba pasa, quiere decir que las delegaciones se agregan correctamente a la lista.
 
 }
---
+++ src/test/java/ouc/fp/ongmanager/XMLTrabajadorDAOTest.java
@@ -0,0 +1,50 @@
+package ouc.fp.ongmanager;
+
+import static org.junit.Assert.*;
+import static org.junit.Assert.assertNull;
+import java.util.ArrayList;
+import java.util.List;
+import javax.xml.bind.JAXBException;
+import org.junit.Test;
+
+//Test para comprobar que el metodo encontrarTrabajadorPorId() funciona correctamente.
+public class XMLTrabajadorDAOTest {
+
+	@Test
+	public void testBorrar() throws JAXBException {
+
+		//Inicializamos cuatro trabajadores que utilizaremos para las pruebas (importante que tengan ID definida).
+		Trabajador t1 = new Trabajador("Jorge", "Masvidal", "22I", null, null, null, null, null, null, null, null);
+		Trabajador t2 = new Trabajador("Maria", "Rodriguez", "82E", null, null, null, null, null, null, null, null);
+		Trabajador t3 = new Trabajador("Sara", "Gomez", "75S", null, null, null, null, null, null, null, null);
+		Trabajador t4 = new Trabajador("Pablo", "Costa", "99M", null, null, null, null, null, null, null, null);
+
+		//Creamos un listado de trabajadores.
+		List<Trabajador> listadoTrabajadores = new ArrayList<Trabajador>();
+
+		//Agregamos los cuatro trabajadores a la lista.
+		listadoTrabajadores.add(t1);
+		listadoTrabajadores.add(t2);
+		listadoTrabajadores.add(t3);
+		listadoTrabajadores.add(t4);
+
+		//Creamos instancia "lt" de la clase "ListadoTrabajadores"
+		ListadoTrabajadores lt = new ListadoTrabajadores();
+		//Asignamos la lista almacenada en "listadoTrabajadores" en la instancia "lt".
+		lt.setListadoTrabajadores(listadoTrabajadores);
+
+		XMLTrabajadorDAO xmlTrabajadorDao = new XMLTrabajadorDAO();
+
+		//Asignamos una instancia "ListadoTrabajadores" al objeto "xmlTrabajadorDao".
+		xmlTrabajadorDao.setListadoTrabajadores(lt);
+
+		//En este caso, el resultado debería de ser un objeto de la clase "Trabajador", por tanto, no debemos recibir "null".
+		assertNotNull(xmlTrabajadorDao.encontrarTrabajadorPorId("82E"));
+
+		//En este caso, el resultado debería de ser "null", al no existir ningun trabajador con esa ID.
+		assertNull(xmlTrabajadorDao.encontrarTrabajadorPorId("822E"));
+	}
+
+	//Si la prueba pasa, quiere decir que el método encuentra correctamente a los trabajadores en una lista mediante su selección por el campo "id".
+	
+}
