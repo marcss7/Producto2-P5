765b4b0072a0a3aaa241f54c58b36d65ddeb4b6c
Teresa Garcia Ceva <tgarciace@fp.uoc.edu>
2019-11-03 10:03:20 +0100

    Agrego ListadoTrabajadoresTest

---
+++ src/test/java/ouc/fp/ongmanager/ListadoTrabajadoresTest.java
@@ -0,0 +1,34 @@
+package ouc.fp.ongmanager;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.xml.bind.JAXBException;
+
+import org.junit.Test;
+
+public class ListadoTrabajadoresTest {
+
+	@Test
+	public void testAdd() throws JAXBException {
+		
+		Trabajador t1 = new Trabajador();
+		Trabajador t2 = new Trabajador();
+		Trabajador t3 = new Trabajador();
+		Trabajador t4 = new Trabajador();
+		
+		List<Trabajador> listadoTrabajadores = new ArrayList<Trabajador>();
+		assertEquals(0, listadoTrabajadores.size());
+		listadoTrabajadores.add(t1);
+		assertEquals(1, listadoTrabajadores.size());
+		listadoTrabajadores.add(t2);
+		assertEquals(2, listadoTrabajadores.size());
+		listadoTrabajadores.add(t3);
+		assertEquals(3, listadoTrabajadores.size());
+		listadoTrabajadores.add(t4);
+		assertEquals(4, listadoTrabajadores.size());
+	}
+
+}
